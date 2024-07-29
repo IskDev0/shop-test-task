38d1be64f4d100603467d43ed0be84a45af3acbf
Iskander <isk.dev001@gmail.com>
2024-07-29 12:17:37 +0500

    add: categories sidebar

---
+++ components/CategoriesSidebar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type {ICategory} from "~/types/ICategory";
+
+const {selectCategory} = useProductsStore()
+
+defineProps<{
+  categories: ICategory[]
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-start gap-3">
+    <button
+        class="font-semibold"
+        @click="selectCategory(category)"
+        v-for="category in categories"
+        :key="category.id">
+      {{ category.name }}
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 const productsStore = useProductsStore()
 const {getProducts} = productsStore
-const {products} = storeToRefs(productsStore)
+const {products, categories} = storeToRefs(productsStore)
 
 </script>
 
 <template>
-<ProductsList :products="products"/>
+<div class="flex justify-between pt-10">
+  <div class="w-1/3">
+    <CategoriesSidebar :categories="categories"/>
+  </div>
+  <div class="w-2/3">
+    <ProductsList :products="products"/>
+  </div>
+</div>
 </template>
